89667f260e451548b777f79d06348f68e372c470
Shivam16Gupta <shivamgupta1281@gmail.com>
2022-06-18 02:25:56 +0530

    added content and style to navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import NavBar from '../src/Components/navbar.js';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <p className="para">lorem Ipsum
+      </p>
     </div>
   );
 }
--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import { useState, useEffect } from "react"
+import '../Assets/css/navbar.css';
 
 function NavBar() {
+    const [navbar, setNavbar] = useState(false);
+
+    const changeBackground = () => {
+        console.log(window.scrollY)
+        if (window.scrollY >= 20) {
+          setNavbar(true)
+        } else {
+          setNavbar(false)
+        }
+      }
+
+    useEffect(() => {
+        changeBackground()
+       
+        window.addEventListener("scroll", changeBackground)
+      })
     return (
         <div>
-            <h1>Hello!!</h1>
+            <div className={navbar ? "navbar-active" : "navbar"}>
+                <div className="elements">
+                    <div className="brand">bhagatbabatejgirihshardoi</div>
+                    <div className="navs">
+                        <span>होम</span>
+                        <span>हमारे बारे में</span>
+                        <span>गैलरी</span>
+                        <span>सम्पर्क सूत्र</span>
+                    </div>
+                </div>
+            </div>
+            
         </div>
     );
 }
