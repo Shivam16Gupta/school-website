0c0848d236e7feba48d0ce825059a41600479356
Shivam16Gupta <shivamgupta1281@gmail.com>
2022-06-21 15:15:40 +0530

    completed about us

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import HomePage from '../src/Components/homepage.js';
 import './App.css';
+import AboutUs from '../src/Components/aboutus.js';
 
 
 function App() {
   return (
     <div className="App">
-      <HomePage/>
+      {/* <HomePage/> */}
+      <AboutUs/>
     </div>
   );
 }
